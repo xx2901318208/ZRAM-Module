908dadbb463174fe4bf94eecb509b8a1784fcb5e
冷川 <138573694+MClengchuan@users.noreply.github.com>
2025-05-25 10:34:46 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,29 +1,35 @@
 #!/system/bin/sh
 
 MODDIR=${0%/*}
+CONFIG_FILE="$MODDIR/config.prop"
 
-# 延迟启动，等待系统稳定
+# 如果配置文件不存在则退出
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "config.prop not found! Exiting."
+  exit 1
+fi
+
+# 加载变量
+. "$CONFIG_FILE"
+
+# 检查变量是否定义
+if [ -z "$ZRAM_ALGO" ] || [ -z "$ZRAM_SIZE" ]; then
+  echo "ZRAM_ALGO or ZRAM_SIZE is not set! Exiting."
+  exit 1
+fi
+
+# 加载zram
 sleep 30
-
-# 模块路径
-ZSTDN_KO="$MODDIR/zram/zstdn.ko"
-ZRAM_KO="$MODDIR/zram/zram.ko"
-
-# 使用 KernelSU 权限执行
-su -c "insmod $ZSTDN_KO"
-su -c "swapoff /dev/block/zram0"
-su -c "rmmod zram"
+su -c insmod $MODDIR/zram/zstdn.ko
+su -c swapoff /dev/block/zram0
+su -c rmmod zram
 sleep 5
-su -c "insmod $ZRAM_KO"
+su -c insmod $MODDIR/zram/zram.ko
 sleep 5
-
-# 配置 zram 参数
-su -c "echo '1' > /sys/block/zram0/reset"
-su -c "echo '0' > /sys/block/zram0/disksize"
-su -c "echo '8' > /sys/block/zram0/max_comp_streams"
-su -c "echo 'lz4kd' > /sys/block/zram0/comp_algorithm"
-su -c "echo '17179869184' > /sys/block/zram0/disksize"
-
-# 启用 swap
-su -c "mkswap /dev/block/zram0 > /dev/null 2>&1"
-su -c "swapon /dev/block/zram0 > /dev/null 2>&1"
+echo '1' > /sys/block/zram0/reset
+echo '0' > /sys/block/zram0/disksize
+echo '8' > /sys/block/zram0/max_comp_streams
+su -c "echo '${ZRAM_ALGO}' > /sys/block/zram0/comp_algorithm"
+su -c "echo '${ZRAM_SIZE}' > /sys/block/zram0/disksize"
+mkswap /dev/block/zram0 > /dev/null 2>&1
+swapon /dev/block/zram0 > /dev/null 2>&1
