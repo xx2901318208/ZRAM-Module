14249214987774690a3444cc409ed8f2d4cb2718
冷川 <138573694+FurLC@users.noreply.github.com>
2025-05-31 06:40:05 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,15 +1,24 @@
-MODPATH=${0%/*}
+# 自动获取模块名称与路径
 MODNAME="$(basename "$MODPATH")"
 OLD_MODPATH="/data/adb/modules/$MODNAME"
+
+# 当前与旧模块 ZRAM 路径
 ZRAM_DIR="$MODPATH/ZRAM"
 OLD_ZRAM_DIR="$OLD_MODPATH/ZRAM"
 
+ui_print "-------------"
+ui_print "  _____           _     ____ "
+ui_print " |  ___|   _ _ __| |   / ___|"
+ui_print " | |_ | | | | '__| |  | |    "
+ui_print " |  _|| |_| | |  | |__| |___ "
+ui_print " |_|   \__,_|_|  |_____\____|"
 ui_print ">> 检查已安装模块的 ZRAM 文件夹是否存在..."
-mkdir "$ZRAM_DIR"
 
 if [ -d "$OLD_ZRAM_DIR" ]; then
-  ui_print ">> 检测到已有 ZRAM 文件夹，复制原文件"
-  cp -af "$OLD_MODPATH/ZRAM/." "$ZRAM_DIR"
+  ui_print ">> 已检测到旧模块 ZRAM 文件夹，复制保留文件中..."
+  mkdir -p "$ZRAM_DIR"
+  cp -af "$OLD_ZRAM_DIR/." "$ZRAM_DIR/"
+  ui_print ">> 文件复制完成 ✅"
 else
-  ui_print ">> 未检测到 ZRAM 文件夹，跳过复制"
+  ui_print ">> 未检测到旧模块 ZRAM 文件夹，跳过复制"
 fi
