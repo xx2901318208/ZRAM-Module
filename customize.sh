fcce0c1cf81efaded84d87b8a51a27de4730a5a9
冷川 <138573694+FurLC@users.noreply.github.com>
2025-05-31 05:05:10 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,13 +1,12 @@
 MODDIR=${0%/*}
 TMPDIR=$MODDIR
-ZRAM_DIR="$MODPATH/zram"
+ZRAM_DIR="$MODPATH/ZRAM"
 
-ui_print ">> 检查 zram 文件夹是否存在..."
+ui_print ">> 检查 ZRAM 文件夹是否存在..."
 
 if [ -d "$ZRAM_DIR" ]; then
-  ui_print ">> 检测到已有 zram 文件夹，保留原内容，跳过覆盖"
+  ui_print ">> 检测到已有 ZRAM 文件夹，保留原内容，跳过覆盖"
 else
-  ui_print ">> 未检测到 zram 文件夹，创建并复制新文件"
+  ui_print ">> 未检测到 ZRAM 文件夹，创建 ZRAM 文件夹"
   mkdir -p "$ZRAM_DIR"
-  cp -af "$TMPDIR/zram/." "$ZRAM_DIR/"
 fi
