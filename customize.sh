5b2b164e9cd75afb434d0fad84cd90fed8357c90
冷川 <138573694+FurLC@users.noreply.github.com>
2025-05-31 04:45:13 +0800

    Update customize.sh
    添加对zram文件夹的检测与创建

--- customize.sh
+++ customize.sh
@@ -1,1 +1,12 @@
-MODDIR=${0%/*}
+MODPATH=$MODPATH
+ZRAM_DIR="$MODPATH/zram"
+
+ui_print ">> 检查 zram 文件夹是否存在..."
+
+if [ -d "$ZRAM_DIR" ]; then
+  ui_print ">> 检测到已有 zram 文件夹，保留原内容，跳过覆盖"
+else
+  ui_print ">> 未检测到 zram 文件夹，创建并复制新文件"
+  mkdir -p "$ZRAM_DIR"
+  cp -af "$TMPDIR/zram/." "$ZRAM_DIR/"
+fi
