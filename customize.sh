036b1e50b82def2f4f3674949f833159287a9706
冷川 <138573694+FurLC@users.noreply.github.com>
2025-05-31 05:57:34 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,12 +1,15 @@
-MODDIR=${0%/*}
-TMPDIR=$MODDIR
+MODPATH=${0%/*}
+MODNAME="$(basename "$MODPATH")"
+OLD_MODPATH="/data/adb/modules/$MODNAME"
 ZRAM_DIR="$MODPATH/ZRAM"
+OLD_ZRAM_DIR="$OLD_MODPATH/ZRAM"
 
-ui_print ">> 检查 ZRAM 文件夹是否存在..."
+ui_print ">> 检查已安装模块的 ZRAM 文件夹是否存在..."
+mkdir "$ZRAM_DIR"
 
-if [ -d "$ZRAM_DIR" ]; then
-  ui_print ">> 检测到已有 ZRAM 文件夹，保留原内容，跳过覆盖"
+if [ -d "$OLD_ZRAM_DIR" ]; then
+  ui_print ">> 检测到已有 ZRAM 文件夹，复制原文件"
+  cp -af "$OLD_MODPATH/ZRAM/." "$ZRAM_DIR"
 else
-  ui_print ">> 未检测到 ZRAM 文件夹，创建 ZRAM 文件夹"
-  mkdir -p "$ZRAM_DIR"
+  ui_print ">> 未检测到 ZRAM 文件夹，跳过复制"
 fi
