1a6f4a606784fac714143c633dcef2802e410ef6
冷川 <138573694+FurLC@users.noreply.github.com>
2025-06-01 08:32:45 +0800

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,2 +1,12 @@
-#!/system/bin/sh
 MODDIR=${0%/*}
+
+# 清理除 zram0 以外的所有 zram 实例
+for zdev in /dev/block/zram*; do
+  zname=$(basename "$zdev")
+  if [ "$zname" != "zram0" ]; then
+    swapoff "/dev/block/$zname" 2>/dev/null
+    echo 1 > "/sys/block/$zname/reset" 2>/dev/null
+    echo 1 > /sys/class/zram-control/hot_remove 2>/dev/null
+    ui_print ">> 已清理多余设备：$zname"
+  fi
+done
