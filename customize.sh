dddfcf71270a7a41f3e088040f8e4ed3c97e4071
冷川 <138573694+FurLC@users.noreply.github.com>
2025-06-03 11:58:40 +0800

    更新 customize.sh

--- customize.sh
+++ customize.sh
@@ -3,8 +3,8 @@ MODNAME="$(basename "$MODPATH")"
 OLD_MODPATH="/data/adb/modules/$MODNAME"
 
 # 当前与旧模块 ZRAM 路径
-ZRAM_DIR="$MODPATH/ZRAM"
-OLD_ZRAM_DIR="$OLD_MODPATH/ZRAM"
+ZRAM_DIR="$MODPATH/zram"
+OLD_ZRAM_DIR="$OLD_MODPATH/zram"
 
 ui_print "-------------"
 ui_print "  _____           _     ____ "
@@ -15,13 +15,13 @@ ui_print " |_|   \__,_|_|  |_____\____|"
 ui_print "      FurLC ZRAM Module      "
 ui_print "-------------"
 
-ui_print ">> 检查已安装模块的 ZRAM 文件夹是否存在..."
+ui_print ">> 检查已安装模块的 zram 文件夹是否存在..."
 
 if [ -d "$OLD_ZRAM_DIR" ]; then
-  ui_print ">> 已检测到旧模块 ZRAM 文件夹，复制保留文件中..."
+  ui_print ">> 已检测到旧模块 zram 文件夹，复制保留文件中..."
   mkdir -p "$ZRAM_DIR"
   cp -af "$OLD_ZRAM_DIR/." "$ZRAM_DIR/"
   ui_print ">> 文件复制完成 ✅"
 else
-  ui_print ">> 未检测到旧模块 ZRAM 文件夹，跳过复制"
+  ui_print ">> 未检测到旧模块 zram 文件夹，跳过复制"
 fi
